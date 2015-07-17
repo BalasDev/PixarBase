ec3c5dbcd23b433fde95e7f1fb88d46ecd75a125
Maxim <test@alfagomel.by>
2015-07-17 14:24:52 +0300

    Working app with 3 forms and working base

---
+++ src/main/java/by/pixar/uvd/web/PersonalController.java
@@ -0,0 +1,44 @@
+package by.pixar.uvd.web;
+
+import by.pixar.uvd.domain.Personal;
+import by.pixar.uvd.service.PersonalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class PersonalController {
+    @Autowired
+   PersonalService personalService;
+    @RequestMapping("/")
+    public String home() {
+        return "hello";
+    }
+    @RequestMapping("/personal")
+    public String listContacts(Map<String, Object> map) {
+
+      //
+        map.put("personalList", personalService.listPersonal());
+
+        return "personal";
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute("personal") Personal personal,
+                             BindingResult result) {
+
+        personalService.addPersonal(personal);
+
+        return "hello";
+    }
+        @RequestMapping("/adds")
+        public String add(Map<String, Object> map) {
+            map.put("personal", new Personal());
+            return "addMent";
+        }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=utf8"
+		 pageEncoding="utf8"%>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+
+<html>
+<body>
+	<h1><a href="/personal">Кто тут мент‚?</a></h1>
+	<h1><a href="/adds">Добавить мента</a></h1>
+</body>
+</html>
